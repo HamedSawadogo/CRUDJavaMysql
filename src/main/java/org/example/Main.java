bbf94c3f27822b6c14b02effc84f2041d46eb7c3
Sawadogo Hamed <102871528+Sawadogoxxx@users.noreply.github.com>
2023-04-27 10:44:23 +0100

    connection avec la base de donnée MYSQL

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,29 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args)  {
+        try {
+            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/employee","root","l0G9IjqGzqZtZSOx");
+            Statement statement=connection.createStatement();
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            ResultSet resultSet=statement.executeQuery("SELECT * FROM employee");
+            ResultSetMetaData metaData=resultSet.getMetaData();
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.println("----------------liste des employees------------------------");
+            while (resultSet.next()){
+                  System.out.println("Nom de l'employee: "+resultSet.getString("first_name"));
+                  System.out.println("prenom de l'employee: "+resultSet.getString("first_name"));
+                  System.out.println("Adresse mail: "+resultSet.getString("email"));
+                  System.out.println("--------------------------------------------------------");
+
+            }
+            resultSet.close();
+            statement.close();
+
+        }catch (SQLException e){
+             System.out.println(e);
         }
     }
 }
